2d1bb1ed28c63e479e6dbb0aabba10bc32a2de51
Justine Ballares <ballaresjustine@gmail.com>
2023-01-20 14:25:57 +0800

    Fix split on question api to add the content

--- api/schema/questions.py
+++ api/schema/questions.py
@@ -1,9 +1,23 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
 from api.model.questions import QuestionsQuestion
 
 
-class QuestionsSchema(SQLAlchemyAutoSchema):
+class QuestionsSchema(SQLAlchemySchema):
 
     class Meta:
         model = QuestionsQuestion
         load_instance = True
+
+    id = auto_field()
+    title = auto_field()
+
+
+class QuestionsSchema2(SQLAlchemySchema):
+
+    class Meta:
+        model = QuestionsQuestion
+        load_instance = True
+
+    id = auto_field()
+    title = auto_field()
+    content = auto_field()
